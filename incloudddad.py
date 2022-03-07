3fda9923bf92358ac85f7d49ff7b5d1b1356c2d0
GrishukViktar <viktorgrishuk04@gmail.com>
2022-03-07 15:31:11 +0300

    frist commit

---
+++ incloudddad.py
@@ -0,0 +1,5 @@
+import time
+print('hello world')
+x = {}
+time.sleep(5)
+print ('get please')
---
+++ php.py
@@ -0,0 +1,11 @@
+import time
+print("ноый проект")
+x =open ('C:\\Users\Виктор\\Documents\\gitcours\\new.txt')
+print (x)
+time.sleep(4)
+print ('the end')
+point = (1, 2, 3, 4, 5)
+for elem in point:
+    if elem == 2:
+        print('Yes')
+time.sleep(2.4)
